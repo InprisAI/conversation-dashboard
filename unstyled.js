3b4dc0b1545c49b1e50e00b7e304e223f78102d9
razawi <raz.kronenberg@gmail.com>
2023-12-27 17:38:33 +0200

    now with phone number

--- unstyled.js
+++ unstyled.js
@@ -1,4 +1,10 @@
+var currentUrl = '';
+const prodURL = 'https://humains-core-dev.appspot.com/dashboard-conv';
+const devURL = 'http://127.0.0.1:5000/dashboard-conv';
+const client_id = 'test:d4n4';
+
 function makeTable(data) {
+
 
   $('#dashboard-table tbody').empty();
   var jsonData = {};
@@ -9,34 +15,39 @@ function makeTable(data) {
       console.error("Error parsing JSON:", error);
   }
 
-  for (i=0; i < jsonData["data"].length; i++) {
-    var rowData = {}
-    var currentRow = jsonData["data"][i];
-    if (currentRow["tag"] == "Customer") {
-      rowData["time"] = currentRow["time"]
-      rowData["speaker"] = currentRow["tag"]
-      rowData["conversation_id"] = jsonData["conversation_id"]
-      rowData["analysis"] = "-"
-      rowData["customer"] = "-"
-      rowData["content"] = currentRow["content"]
-    }
-    else {  
-      rowData["time"] = currentRow["time"]
-      rowData["speaker"] = currentRow["tag"]
-      rowData["conversation_id"] = jsonData["conversation_id"]
-      rowData["analysis"] = currentRow["content"]["json_metadata"]["analysis"]
-      rowData["customer"] = currentRow["content"]["json_metadata"]["customer"]
-      rowData["content"] = currentRow["content"]["convers_content"]
-    }
+  var keys = Object.keys(jsonData["data"]);
+  for (var j = 0; j < keys.length; j++) {
+    var key = keys[j];
+    for (i=0; i < jsonData["data"][key].length; i++) {
+      var rowData = {}
+      var currentRow = jsonData["data"][key][i];
+      if (currentRow["tag"] == "Customer") {
+        rowData["time"] = currentRow["time"]
+        rowData["speaker"] = currentRow["tag"]
+        rowData["conversation_id"] = key
+        rowData["analysis"] = "-"
+        rowData["customer"] = "-"
+        rowData["content"] = currentRow["content"]
+      }
+      else {  
+        rowData["time"] = currentRow["time"]
+        rowData["speaker"] = currentRow["tag"]
+        rowData["conversation_id"] = key
+        rowData["analysis"] = currentRow["content"]["json_metadata"]["analysis"]
+        rowData["customer"] = currentRow["content"]["json_metadata"]["customer"]
+        rowData["content"] = currentRow["content"]["convers_content"]
+      }
+    
   
-    // Create a new row element and populate it with the data
-    var newRow = $("<tr>");
-    $.each(rowData, function(key, value) {
-        newRow.append($("<td>").text(value));
-    });
-  
-    // Append the new row to the table's tbody
-    $("#dashboard-table tbody").append(newRow);
+      // Create a new row element and populate it with the data
+      var newRow = $("<tr>");
+      $.each(rowData, function(key, value) {
+          newRow.append($("<td>").text(value));
+      });
+    
+      // Append the new row to the table's tbody
+      $("#dashboard-table tbody").append(newRow);
+    }
   }
 }
 
@@ -57,17 +68,25 @@ function fetchData(url) {
     });
 }
 
+function refreshData() {
+  fetchData(currentUrl);
+}
+
 $("#refresh-button").click(function () {
-
-  const prodURL = 'https://humains-core-dev.appspot.com/dashboard-conv' 
-  const devURL = 'http://127.0.0.1:5000/dashboard-conv' 
-  const params = {
-    conversation_id : 'bvydjzvbyfjdgdX', //'GcD67kmvcdvuKvfcyJyyDvh',
-    client_id : 'test:d4n4',
-    state_name : 'D4N4_CHAT_STATE'
-  }
-
-  fullURL = `${prodURL}/${params["client_id"]}/${params["conversation_id"]}/${params["state_name"]}`
-  
-  fetchData(fullURL);
+  refreshData()
 });
+
+$("#search-button").click(function () {
+  // todo - get the data from search-input
+  // determine if its phone number or IdleDeadlinecreate querystring and fetch
+  var serchString = $('#search-input')[0].value
+  var pattern = /[A-Za-z]/;
+  if (pattern.test(serchString)) {
+    currentUrl = `${prodURL}?client_id=${client_id}&conversation_id=${serchString}`
+  } else {
+    currentUrl = `${prodURL}?client_id=${client_id}&phone_number=${serchString}`
+  } 
+  fetchData(currentUrl);
+});
+
+var intervalId = setInterval(refreshData, 5000);
